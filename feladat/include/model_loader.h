283111c8dd61bff5238b91874a53fb40e8a10c7f
fzksroller <fzksroller@gmail.com>
2026-04-29 22:45:10 +0200

    commit hiba?

--- feladat/include/model_loader.h
+++
@@ -1,9 +0,0 @@
-#ifndef MODEL_LOADER_H
-#define MODEL_LOADER_H
-
-#include "model.h"
-
-Model* loadOBJ(const char* path);
-void freeModel(Model* model);
-
-#endif
--- feladat/include/shadow.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHADOW_H
-#define SHADOW_H
-
-#include <GL/gl.h>
-
-void shadowMatrix(GLfloat shadowMat[16], GLfloat ground[4], GLfloat light[4]);
-
-#endif
